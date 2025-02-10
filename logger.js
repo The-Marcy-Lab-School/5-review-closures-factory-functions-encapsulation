362670271fff2d7f01e627da7cbcfa30d4a03a3a
Ben Spector <111444562+benspector-mls@users.noreply.github.com>
2025-02-10 10:30:19 -0500

    Update logger.js

--- logger.js
+++ logger.js
@@ -7,5 +7,5 @@ errorLogger.log("Something went wrong!"); // [ERROR] Something went wrong!
 const warningLogger = createLogger("[WARNING]");
 warningLogger.log("You are almost out of storage."); // [WARNING] You are almost out of storage.
 
-const errorLogger = createLogger("[INFO]");
-errorLogger.log("Learn more about closure at MDN."); // [INFO] Learn more about closure at MDN.
+const infoLogger = createLogger("[INFO]");
+infoLogger.log("Learn more about closure at MDN."); // [INFO] Learn more about closure at MDN.
