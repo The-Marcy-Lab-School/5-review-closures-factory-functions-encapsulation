37901245bdf97524dc94cd17ca948c6c0d2f5d69
Ben Spector <111444562+benspector-mls@users.noreply.github.com>
2025-02-10 09:03:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const createCounter = () => {
+  let count = 0; // Private variable
+  
+  return {
+    increment: () => count++,
+    decrement: () => count--,
+    getCount: () => count
+  };
+};
 
+const counter = createCounter();
+console.log(counter.getCount()); // 0
+counter.increment();
+console.log(counter.getCount()); // 1
